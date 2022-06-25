8f2cb90b948afb6cab37af4c82ea8c1fe40ad66d
sorrybabyxxwh <99652065+sorrybabyxxwh@users.noreply.github.com>
2022-06-25 21:46:01 +0300

    h

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 """
 Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
 Source: https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/echobot2.py
@@ -7,6 +6,8 @@ Source: https://github.com/python-telegram-bot/python-telegram-bot/blob/master/e
 import logging
 import os
 
+import requests
+from lxml import html
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Enable logging
@@ -17,27 +18,38 @@ logger = logging.getLogger(__name__)
 TOKEN = '5554400453:AAEaYLAkrAprOwqn2NGDkiqi6QzZJVCCcsw'
 PORT = int(os.environ.get('PORT', 5000))
 
+
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
 
+
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
+
 def get(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Haha!')
+    r = requests.get("https://anekdoty.net.ua/category/anekdoty")
+    tree = html.fromstring(r.content)
+    a = tree.xpath('.//div[@ class="entry-summary entry-sub-title"]//text()')
+    while a > 0:
+        update.message.reply_text(a[1])
+        a += 1
+
 
 def echo(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
+
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 def main():
     """Start the bot."""
@@ -70,6 +82,7 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
